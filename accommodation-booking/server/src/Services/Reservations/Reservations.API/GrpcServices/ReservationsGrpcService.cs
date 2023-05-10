2a7d0d99c20e431822708d6f40ab0e00cb9dc5cc
markor33 <markorpc1@gmail.com>
2023-05-11 01:07:04 +0200

    Implement grpc add-accommodation

--- accommodation-booking/server/src/Services/Reservations/Reservations.API/GrpcServices/ReservationsGrpcService.cs
+++ accommodation-booking/server/src/Services/Reservations/Reservations.API/GrpcServices/ReservationsGrpcService.cs
@@ -1,16 +1,31 @@
 ﻿using Grpc.Core;
 using GrpcReservations;
+using ReservationsLibrary.Models;
 using ReservationsLibrary.Services;
 
 namespace Reservations.API.GrpcServices
 {
     public class ReservationsGrpcService : GrpcReservations.Reservations.ReservationsBase
     {
+        private readonly IAccommodationService _accommodationService;
         private readonly IReservationService _reservationService;
 
-        public ReservationsGrpcService(IReservationService reservationService)
+        public ReservationsGrpcService(IReservationService reservationService, IAccommodationService accommodationService)
         {
             _reservationService = reservationService;
+            _accommodationService = accommodationService;
+        }
+
+        public override async Task<AddAccommodationResponse> AddAccommodation(AddAccommodationRequest request, ServerCallContext context)
+        {
+            var accommodation = new Accommodation()
+            {
+                Id = Guid.Parse(request.AccommodationId),
+                HostId = Guid.Parse(request.HostId),
+                AutoConfirmation = request.AutoConfirmation,
+            };
+            _accommodationService.Create(accommodation);
+            return new AddAccommodationResponse();
         }
 
         public async override Task<CheckActiveReservationsResponse> CheckActiveReservations(CheckActiveReservationsRequest request, ServerCallContext context)
